0d7e5976d3bd6d341da93778ecc9755be45e2422
Nemo Semret <nemo@gro-intelligence.com>
2017-10-18 15:07:32 -0400

    example with actual data series randomly selected from available

--- gro_client.py
+++ gro_client.py
@@ -1,5 +1,6 @@
 import argparse
 import sys
+from random import random
 from api.client.lib import get_data, get_access_token
 
 API_HOST = 'apidev11201.gro-intelligence.com'
@@ -10,21 +11,34 @@ def get_available(access_token, entity_type):
     'regions', returns a JSON dict with the list of available entities
     of the given type.
     """
-    url = '/'.join(['https:', '', API_HOST, 'v2', 'available', entity_type])
+    url = '/'.join(['https:', '', API_HOST, 'v2/available', entity_type])
     headers = {'authorization': 'Bearer ' + access_token}
     resp = get_data(url, headers)
-    assert resp.status_code == 200, 'Request failed: {}'.format(resp)
     return resp.json()
 
 
-def get_data_series(access_token, item_id, metric_id, region_id, frequency, source_id):
-    url = '/'.join(['https:', '', API_HOST, 'v2', 'data'])
+def list_available(access_token, params):
+    url = '/'.join(['https:', '', API_HOST, 'v2/entities/list'])
     headers = {'authorization': 'Bearer ' + access_token}
-    params = { 'regionId': region_id, 'itemId': item_id, 'metricId': metric_id,
-               'frequency': frequency, 'sourceId': source_id}
+    resp = get_data(url, headers, params)
+    return resp.json()['data']
+
+
+def get_data_series(access_token, item_id, metric_id, region_id):
+    url = '/'.join(['https:', '', API_HOST, 'v2/data_series/list'])
+    headers = {'authorization': 'Bearer ' + access_token}
+    params = { 'regionId': region_id, 'itemId': item_id, 'metricId': metric_id}
     resp = get_data(url, headers, params, lambda x: sys.stderr.write(str(x)))
-    assert resp.status_code == 200, 'Request failed: {}'.format(resp)
-    return resp.json()
+    return resp.json()['data']
+
+
+def get_data_points(access_token, item_id, metric_id, region_id, frequency, source_id):
+    url = '/'.join(['https:', '', API_HOST, 'v2/data'])
+    headers = {'authorization': 'Bearer ' + access_token }
+    params = {'regionId': region_id, 'itemId': item_id, 'metricId': metric_id,
+              'frequencyId': frequency, 'sourceId': source_id}
+    resp = get_data(url, headers, params, lambda x: sys.stderr.write(str(x)))
+    return resp.json()['data']
 
 
 if __name__ == "__main__":
@@ -35,11 +49,31 @@ if __name__ == "__main__":
 
     access_token = get_access_token(API_HOST, args.user_email, args.user_password)
 
-    # Find all items that have some data series
-    print get_available(access_token, 'items')
-    # Find all metrics that have some data series
-    print get_available(access_token, 'metrics')
+    # Find some random item that has data series
+    selected_entities = {}
+    for item in  get_available(access_token, 'items'):
+        if random() > 0.5:
+            print "Randomly selected item: {}".format(item['name'])
+            selected_entities['itemId'] = item['id']
+            break
 
+    # Find all metrics that have some data series
+    for entities in list_available(access_token, selected_entities):
+        if random() > 0.1:
+            print "Using entities: {}".format(str(entities))
+            selected_entities.update(entities)
+            break
 
     # Random data series examples
-    print get_data_series(access_token, 63, 860032, 1001, 'annual', 2)
+    for data_series in get_data_series(access_token,
+                                       selected_entities['item_id'],
+                                       selected_entities['metric_id'],
+                                       selected_entities['region_id']):
+        print "Using data series: {}".format(data_series['series_id'])
+        print get_data_points(access_token,
+                              data_series['item_id'],
+                              data_series['metric_id'],
+                              data_series['region_id'],
+                              data_series['frequency_id'],
+                              data_series['source_id'])
+        break
