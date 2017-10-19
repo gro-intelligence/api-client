6115399ce7c141a2a0e9a2cc9e5b5ffb41c7b283
Nemo Semret <nemo@gro-intelligence.com>
2017-10-19 09:40:39 -0400

    modify example randomly pick item uniformly from all items, and metric and region for it also uniformly

--- gro_client.py
+++ gro_client.py
@@ -16,18 +16,16 @@ if __name__ == "__main__":
 
     # Pick a random item that has some data associated with it.
     selected_entities = {}
-    for item in  get_available(access_token, API_HOST, 'items'):
-        if random() > 0.5:
-            print "Randomly selected item: {}".format(item['name'])
-            selected_entities['itemId'] = item['id']
-            break
+    item_list = get_available(access_token, API_HOST, 'items')
+    item = item_list[int(len(item_list)*random())]
+    print "Randomly selected item: {}".format(item['name'])
+    selected_entities['itemId'] = item['id']
 
-    # Find all metrics that have some data series
-    for entities in list_available(access_token, API_HOST, selected_entities):
-        if random() > 0.1:
-            print "Using entities: {}".format(str(entities))
-            selected_entities.update(entities)
-            break
+    # Pick a random metric and region for this item with data available
+    entity_list = list_available(access_token, API_HOST, selected_entities)
+    entities = entity_list[int(len(entity_list)*random())]
+    print "Using entities: {}".format(str(entities))
+    selected_entities.update(entities)
 
     # Random data series examples
     for data_series in get_data_series(access_token, API_HOST,
@@ -35,10 +33,12 @@ if __name__ == "__main__":
                                        selected_entities['metric_id'],
                                        selected_entities['region_id']):
         print "Using data series: {}".format(data_series['series_id'])
-        print get_data_points(access_token, API_HOST,
+        for point in get_data_points(access_token, API_HOST,
                               data_series['item_id'],
                               data_series['metric_id'],
                               data_series['region_id'],
                               data_series['frequency_id'],
-                              data_series['source_id'])
+                              data_series['source_id']):
+            print ','.join(map(lambda x: str(x),
+                               [point['start_date'], point['end_date'], point['value']]))
         break
