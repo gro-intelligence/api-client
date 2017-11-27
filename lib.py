339864809c461ec5ffc70395ed84828f46b1eaba
Nemo Semret <nemo@gro-intelligence.com>
2017-11-27 13:28:07 -0500

    Merge remote-tracking branch 'origin/development' into CLEWS-5032

--- lib.py
+++ lib.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 
 MAX_RETRIES = 4
-DEFAULT_LOG_LEVEL=logging.WARNING  # change WARNING to DEBUG for more detail
+DEFAULT_LOG_LEVEL=logging.DEBUG  # change to WARNING for less detail
 
 
 def get_default_logger():
