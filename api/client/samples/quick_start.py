53f7120beee8dc5328e4128e7f9f6143dd324a50
John Ward <john.ward@gro-intelligence.com>
2018-07-12 20:38:37 +0000

    Add get_data_series back into quickstart and update comments

--- api/client/samples/quick_start.py
+++ api/client/samples/quick_start.py
@@ -8,21 +8,27 @@ ACCESS_TOKEN=os.environ['GROAPI_TOKEN']
 
 def main():
     client = api.client.Client(API_HOST, ACCESS_TOKEN)
-    selected_entities = { u'region_id': 1038,
-                          u'item_id': 5187,
-                          u'metric_id': 5590032,
-                          u'source_id': 2,
-                          u'frequency_id': 9,
-                          u'start_date': '2000-01-01',
-                          u'end_date': '2016-12-31' }
+
+    selected_entities = { u'region_id': 1038, # Cape Verde
+                          u'item_id': 5187, # Management of donkey manure
+                          u'metric_id': 5590032 } # Total Emissions Quantity (mass)
+
     writer = unicodecsv.writer(open(OUTPUT_FILENAME, 'wb'))
-    for point in client.get_data_points(**selected_entities):
-        writer.writerow([
-            point['start_date'],
-            point['end_date'],
-            point['value'],
-            client.lookup_unit_abbreviation(point['input_unit_id'])
-        ])
+
+    # Get what possible series there are for that combination of selections
+    for data_series in client.get_data_series(**selected_entities):
+        
+        # Add a time range restriction to your data request (Optional - otherwise get all points)
+        data_series['start_date'] = '2000-01-01'
+        data_series['end_date'] = '2012-12-31'
+
+        for point in client.get_data_points(**data_series):
+            writer.writerow([
+                point['start_date'],
+                point['end_date'],
+                point['value'],
+                client.lookup_unit_abbreviation(point['input_unit_id'])
+            ])
 
 
 if __name__ == "__main__":
