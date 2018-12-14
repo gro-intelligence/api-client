18f2704914611638592d5bf31891459cf52ce562
Eyvind Niklasson <eyvind.niklasson@gro-intelligence.com>
2018-12-14 03:15:56 -0500

    Completely refactored lib.py to include batch query versions of functions. - lib.py now uses Tornado HTTPClient instead of requests library - a number of functions in lib.py now have batch asynchronous versions using a newly added decorator

--- __init__.py
+++ __init__.py
@@ -14,11 +14,23 @@ class Client(object):
     def get_available(self, entity_type):
         return lib.get_available(self.access_token, self.api_host, entity_type)
 
+    def batch_async_get_available(self, list_of_args, output_list=None, map_result=None):
+        return lib.batch_async_get_available(self.access_token, self.api_host, list_of_args,
+                                             output_list=output_list, map_result=map_result)
+
     def list_available(self, selected_entities):
         return lib.list_available(self.access_token, self.api_host, selected_entities)
 
+    def batch_async_list_available(self, list_of_args, output_list=None, map_result=None):
+        return lib.batch_async_list_available(self.access_token, self.api_host, list_of_args,
+                                              output_list=output_list, map_result=map_result)
+
     def lookup(self, entity_type, entity_id):
         return lib.lookup(self.access_token, self.api_host, entity_type, entity_id)
+
+    def batch_async_lookup(self, list_of_args, output_list=None, map_result=None):
+        return lib.batch_async_lookup(self.access_token, self.api_host, list_of_args,
+                                              output_list=output_list, map_result=map_result)
 
     def lookup_unit_abbreviation(self, unit_id):
         """Wrapper to lookup unit names, with local cache to avoid repeated lookups."""
@@ -29,12 +41,24 @@ class Client(object):
     def get_data_series(self, **selection):
         return lib.get_data_series(self.access_token, self.api_host, **selection)
 
+    def batch_async_get_data_series(self, list_of_args, output_list=None, map_result=None):
+        return lib.batch_async_get_data_series(self.access_token, self.api_host, list_of_args,
+                                               output_list=output_list, map_result=map_result)
+
     def get_data_points(self, **selection):
         return lib.get_data_points(self.access_token, self.api_host, **selection)
+
+    def batch_async_get_data_points(self, list_of_args, output_list=None, map_result=None):
+        return lib.batch_async_get_data_points(self.access_token, self.api_host, list_of_args,
+                                               output_list=output_list, map_result=map_result)
 
     def search(self, entity_type, search_terms):
         return lib.search(self.access_token, self.api_host,
                           entity_type, search_terms)
+
+    def batch_async_search(self, list_of_args, output_list=None, map_result=None):
+        return lib.batch_async_search(self.access_token, self.api_host, list_of_args,
+                                      output_list=output_list, map_result=map_result)
 
     def search_and_lookup(self, entity_type, search_terms):
         return lib.search_and_lookup(self.access_token, self.api_host,
@@ -50,3 +74,7 @@ class Client(object):
 
     def get_geo_centre(self, region_id):
         return lib.get_geo_centre(self.access_token, self.api_host, region_id)
+
+    def batch_async_get_geo_centre(self, list_of_args, output_list=None, map_result=None):
+        return lib.batch_async_get_geo_centre(self.access_token, self.api_host, list_of_args,
+                                              output_list=output_list, map_result=map_result)
