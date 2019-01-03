042a45feafb7de2ab6f0f5d6313aad4c9d3c11a5
John Ward <john.ward@gro-intelligence.com>
2019-01-03 18:48:48 +0000

    add setup file

---
+++ setup.py
@@ -0,0 +1,29 @@
+# python setup.py sdist bdist_wheel
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "gro",
+    version = "1.12.0",
+    description = "A client library for accessing Gro Intelligence's agriculture data platform",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/gro-intelligence/api-client",
+    packages = ['api'],
+    python_requires = ">=2.7.6",
+    install_requires = [
+      'unicodecsv',
+      'numpy',
+      'pandas',
+      'python-dateutil',
+      'pytz',
+      'certifi',
+      'chardet',
+      'requests',
+      'urllib3',
+      'future',
+    ]
+)
