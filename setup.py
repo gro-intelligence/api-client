cf2ee3dd4bbb84e9dbd39cbfe56a71798634188b
John Ward <john.ward@gro-intelligence.com>
2019-01-04 23:45:51 +0000

    Re-add api.client package

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ setuptools.setup(
     long_description = long_description,
     long_description_content_type = "text/markdown",
     url = "https://github.com/gro-intelligence/api-client",
-    packages = [ 'api.client' ],
+    packages = [ 'api', 'api.client' ],
+    py_modules = [ 'api.client.lib' ],
     python_requires = ">=2.7.6",
     install_requires = [
       'unicodecsv',
