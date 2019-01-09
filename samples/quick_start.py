a9d236fa767f1bce4232e9f167385cf68d820f3c
James Maringa <james.maringa@gro-intelligence.com>
2019-01-09 11:24:58 +0300

    Add script to drop the old constraints

--- samples/quick_start.py
+++ samples/quick_start.py
@@ -9,18 +9,29 @@ ACCESS_TOKEN=os.environ['GROAPI_TOKEN']
 def main():
     client = api.client.Client(API_HOST, ACCESS_TOKEN)
 
-    selected_entities = { u'region_id': 1038, # Cape Verde
-                          u'item_id': 5187, # Management of donkey manure
-                          u'metric_id': 5590032 } # Total Emissions Quantity (mass)
+    selected_entities = {u'frequency_id': 3,
 
+            u'input_unit_id': 189,
+
+            u'item_id': 321,
+
+            u'metric_id': 70029,
+
+            'region_id': 13074,
+
+            u'show_revisions': False,
+
+            u'source_id': 3,
+
+            'start_date': '2018-12-10'}
     writer = unicodecsv.writer(open(OUTPUT_FILENAME, 'wb'))
 
     # Get what possible series there are for that combination of selections
     for data_series in client.get_data_series(**selected_entities):
-        
+
         # Add a time range restriction to your data request (Optional - otherwise get all points)
-        data_series['start_date'] = '2000-01-01'
-        data_series['end_date'] = '2012-12-31'
+        # data_series['start_date'] = '2000-01-01'
+        # data_series['end_date'] = '2012-12-31'
 
         for point in client.get_data_points(**data_series):
             writer.writerow([
