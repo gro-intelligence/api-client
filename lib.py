75a1b0038f0b2b473af3b03e27eb0b7ce130b2ae
Eyvind Niklasson <eyvind.niklasson@gro-intelligence.com>
2019-01-25 13:30:02 -0500

    fix LINT issue in lib.py

--- lib.py
+++ lib.py
@@ -52,7 +52,7 @@ def get_access_token(api_host, user_email, user_password, logger=None):
       logger.debug("Authentication succeeded in get_access_token")
       return json_decode(login.body)['data']['accessToken']
     else:
-      logger.warning("Error in get_access_token: {}".format(get_api_token))
+      logger.warning("Error in get_access_token: {}".format(login.body))
     retry_count += 1
   raise Exception("Giving up on get_access_token after {0} tries.".format(retry_count))
 
