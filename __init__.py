471349c08504f432dde52a24322aa7fc7724d2f6
Nemo Semret <nemo@gro-intelligence.com>
2019-02-04 17:23:29 -0500

    generate get_provinces to get_descendant_regions

--- __init__.py
+++ __init__.py
@@ -50,3 +50,8 @@ class Client(object):
 
     def get_geo_centre(self, region_id):
         return lib.get_geo_centre(self.access_token, self.api_host, region_id)
+
+    def get_descendant_regions(self, region_id, descendant_level=None):
+        return lib.get_descendant_regions(self.access_token, self.api_host,
+                                          region_id, descendant_level)
+
