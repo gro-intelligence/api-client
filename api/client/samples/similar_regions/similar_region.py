6d613c3e5bab6c8f21f37b1370b488e971bfd308
Nemo Semret <nemo@gro-intelligence.com>
2019-02-08 17:19:37 -0500

    use client obj when getting available regions to compare

--- api/client/samples/similar_regions/similar_region.py
+++ api/client/samples/similar_regions/similar_region.py
@@ -28,7 +28,7 @@ class SimilarRegion(object):
     def _regions_avail_for_selection(self, region_properties):
         regions = set()
         for props in region_properties.values():
-            for available_series in self.list_available(props["selected_entities"]):
+            for available_series in self.client.list_available(props["selected_entities"]):
                 regions.add(available_series["region_id"])
         self._logger.info("{} regions are available for comparison.".format(len(regions)))
         return list(regions)
