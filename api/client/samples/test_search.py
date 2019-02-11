7bc144f460c3b19d6ca8ba71a12ea36aa8a233ef
Simon Bloch <simon@gro-intelligence.com>
2019-02-11 20:02:03 +0000

    escape special solr characters

--- api/client/samples/test_search.py
+++ api/client/samples/test_search.py
@@ -2,7 +2,7 @@ import argparse
 import os
 import api.client as gro_client
 
-ENV = 'dev'
+ENV = 'local'
 
 API_HOSTS = {
     'local': '127.0.0.1:5000',
@@ -22,20 +22,53 @@ API_HOST = API_HOSTS[ENV]
 ACCESS_TOKEN = ACCESS_TOKENS[ENV]
 
 MAX_NUM_RESULTS = 20
-
+KEYS_90210 = {
+    90211: 'Aaap + aaa',
+    90212: 'Aaap - aaa',
+    90213: 'Aaap && aaa',
+    90214: 'Aaap || aaa',
+    90215: 'Aaap ! aaa',
+    90216: 'Aaap ( aaa',
+    90217: 'Aaap ) aaa',
+    90218: 'Aaap { aaa',
+    90219: 'Aaap } aaa',
+    90220: 'Aaap [ aaa',
+    90221: 'Aaap ] aaa',
+    90222: 'Aaap ^ aaa',
+    90223: 'Aaap " aaa',
+    90224: 'Aaap ~ aaa',
+    90225: 'Aaap * aaa',
+    90226: 'Aaap ? aaa',
+    90227: 'Aaap : aaa',
+    90228: 'Aaap \  aaa',
+    90229: 'Aaap ; aaa',
+    90229: 'Aaap | aaa',
+    90229: 'Aaap & aaa'
+}
+    
 
 def run_universal_search_test():
     print "RUNNING 'universal' SEARCH"
+
     while True:
         print '\n\n-------------------------'
         term = raw_input("What's your search term?: ")
+        if not term:
+            print '\n---You must specify a search term!---'
+            continue
         id_results = gro_client.lib.universal_search(
             ACCESS_TOKEN, API_HOST, term)[:MAX_NUM_RESULTS]
         for result in id_results:
-              print '{}: {}'.format(
-                result[1],
-                gro_client.lib.lookup(ACCESS_TOKEN, API_HOST, result[1]+'s',
-                                      result[0])['name'].encode('utf-8'))
+            try:
+                print '{}: {}'.format(
+                    result[1],
+                    gro_client.lib.lookup(ACCESS_TOKEN, API_HOST, result[1]+'s',
+                                          result[0])['name'].encode('utf-8'))
+            except:
+                name = KEYS_90210[result[0]] if result[0] in KEYS_90210 else ''
+                print result, name
+            
+
 
 def run_search_by_type_test(entity_type):
     type_formatted = entity_type if entity_type[-1] == 's' else entity_type+'s'
@@ -45,6 +78,9 @@ def run_search_by_type_test(entity_type):
     while True:
         print '\n\n-------------------------'
         term = raw_input("What's your search term?: ")
+        if not term:
+            print '\n---You must specify a search term!---'
+            continue
         count = 0
         for i in client.search_and_lookup(type_formatted, term):
             print i['name']
