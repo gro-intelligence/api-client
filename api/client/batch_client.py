077f95041940fc3646189bd909b75e948c174e48
John Ward <john.ward@gro-intelligence.com>
2019-03-19 22:04:31 +0000

    Add rate limit lock to batch client

--- api/client/batch_client.py
+++ api/client/batch_client.py
@@ -1,6 +1,6 @@
 import time
 
-#Python3 support
+# Python3 support
 try:
     # Python3
     from urllib.parse import urlencode
@@ -13,17 +13,19 @@ from tornado.escape import json_decode
 from tornado.httpclient import AsyncHTTPClient, HTTPRequest
 from tornado.ioloop import IOLoop
 from tornado.queues import Queue
-
+from tornado.locks import Event
 from api.client import cfg, lib, Client
+
+rate_limit_lock = Event()
+rate_limit_lock.set()
 
 
 class BatchClient(Client):
-
-    """API client with support for batch asynchronous queries. """
+    """API client with support for batch asynchronous queries."""
 
     _logger = None
     _http_client = None
-    
+
     def __init__(self, api_host, access_token):
         super(BatchClient, self).__init__(api_host, access_token)
         self._logger = lib.get_default_logger()
@@ -31,7 +33,10 @@ class BatchClient(Client):
 
     @gen.coroutine
     def get_data(self, url, headers, params=None):
-        """General 'make api request' function. Assigns headers and builds in retries and logging."""
+        """General 'make api request' function.
+
+        Assigns headers and builds in retries and logging.
+        """
         if params is not None:
             url = '{url}?{params}'.format(url=url, params=urlencode(params))
         base_log_record = dict(route=url, params=params)
@@ -40,7 +45,8 @@ class BatchClient(Client):
         while retry_count < cfg.MAX_RETRIES:
             start_time = time.time()
             http_request = HTTPRequest(url, method="GET", headers=headers,
-                                       request_timeout=cfg.TIMEOUT, connect_timeout=cfg.TIMEOUT)
+                                       request_timeout=cfg.TIMEOUT,
+                                       connect_timeout=cfg.TIMEOUT)
             data = yield self._http_client.fetch(http_request)
             elapsed_time = time.time() - start_time
             log_record = dict(base_log_record)
@@ -53,6 +59,10 @@ class BatchClient(Client):
             retry_count += 1
             if retry_count < cfg.MAX_RETRIES:
                 self._logger.warning(data.text, extra=log_record)
+                if data.status_code == 429:
+                    rate_limit_lock.clear()
+                    time.sleep(2 ** retry_count)  # Exponential backoff
+                    rate_limit_lock.set()
             else:
                 self._logger.error(data.text, extra=log_record)
                 raise Exception('Giving up on {} after {} tries. Error is: {}.'.format(
@@ -64,27 +74,31 @@ class BatchClient(Client):
         of: item_id, metric_id, region_id, frequency_id, source_id,
         partner_region_id. Additional arguments are allowed and ignored.
         """
-        headers = {'authorization': 'Bearer ' + self.access_token }
+        headers = {'authorization': 'Bearer ' + self.access_token}
         url = '/'.join(['https:', '', self.api_host, 'v2/data'])
         params = lib.get_data_call_params(**selection)
         resp = yield self.get_data(url, headers, params)
         raise gen.Return(json_decode(resp))
 
-    def batch_async_get_data_points(self, batched_args, output_list=None, map_result=None):
-        return self.batch_async_queue(self.get_data_points, batched_args, output_list, map_result)
+    def batch_async_get_data_points(self, batched_args, output_list=None,
+                                    map_result=None):
+        return self.batch_async_queue(self.get_data_points, batched_args,
+                                      output_list, map_result)
 
     def batch_async_queue(self, func, batched_args, output_list, map_result):
-        """
-        Asynchronously call func
+        """Asynchronously call func.
+
         :param func: function to be called on each member of batched_args
-        :param batched_args: list of keyword arguments dictionaries, one for each call to func
+        :param batched_args: list of keyword arguments dictionaries, one for
+        each call to func
         :param output_list:
         :param map_result:
         :return:
         """
         assert type(batched_args) is list, \
-            "Only argument to a batch async decorated function should be a list of a list of the individual " \
-            "non-keyword arguments being passed to the original function."
+            "Only argument to a batch async decorated function should be a \
+            list of a list of the individual non-keyword arguments being \
+            passed to the original function."
 
         # Default is identity mapping into results list.
         if not map_result:
@@ -98,7 +112,7 @@ class BatchClient(Client):
 
         @gen.coroutine
         def consumer():
-            """Consume the queue by executing func on all items in queue asynchronously."""
+            """Execute func on all items in queue asynchronously."""
             while q.qsize():
                 idx, item = q.get().result()
                 self._logger.debug('Doing work on {}'.format(idx))
@@ -116,7 +130,8 @@ class BatchClient(Client):
             for idx, item in enumerate(batched_args):
                 q.put((idx, item))
             elapsed = time.time() - lasttime
-            self._logger.info("Queued {} requests in {}  ".format(q.qsize(), elapsed))
+            self._logger.info("Queued {} requests in {}".format(q.qsize(),
+                                                                elapsed))
 
         @gen.coroutine
         def main():
@@ -129,4 +144,3 @@ class BatchClient(Client):
         IOLoop.current().run_sync(main)
 
         return output_list
-
