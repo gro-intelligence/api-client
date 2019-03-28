4b43bda0f24045db37140e6bb3dcef43dde3734d
John Ward <john.ward@gro-intelligence.com>
2019-03-28 17:36:57 +0000

    Update quickstart and cash_crops to use GroClient

--- api/client/samples/crop_models/cash_crops.py
+++ api/client/samples/crop_models/cash_crops.py
@@ -25,180 +25,187 @@ through x12. They hold different data as per the comments above each query.
 12.Gro Yield Model (data format is N/A?)
 '''
 
-import numpy as np
-import pandas as pd
-import sys
-import getpass
-import random
-import logging
 import os
 
-import api.client.lib
 from api.client.gro_client import get_df
-from api.client.lib import search
+from api.client.gro_client import GroClient
 
-#GRO GLOBAL VARIABLES
-API_HOST              = 'api.gro-intelligence.com'
+# GRO GLOBAL VARIABLES
+API_HOST = 'api.gro-intelligence.com'
+ACCESS_TOKEN = os.environ['GROAPI_TOKEN']
 
-#METRIC_ID GLOBALS
-YIELD                 =   170037
-HARV_AREA             =   570001
-TEMPERATURE           =  2540047
-VEG_ANOMALIES         =   430029
-ETA_PCT               =  2010042
-PRODUCER_PRICE        =  2290065
-GRINDINGS             =  5380032
-COMPOSITE_PRICES      =  2270065
-ET_VALUE              =  4660031
-LAND_COVER            =  2120042
-SOIL_AVAIL            = 15531082
-WHOLESALE_PX          =  2310065
+# METRIC_ID GLOBALS
+YIELD = 170037
+HARV_AREA = 570001
+TEMPERATURE = 2540047
+VEG_ANOMALIES = 430029
+ETA_PCT = 2010042
+PRODUCER_PRICE = 2290065
+GRINDINGS = 5380032
+COMPOSITE_PRICES = 2270065
+ET_VALUE = 4660031
+LAND_COVER = 2120042
+SOIL_AVAIL = 15531082
+WHOLESALE_PX = 2310065
 
-#ITEM_ID GLOBALS
-NDVI_DIFF_10YR_MEAN   =  409
-ETA_PCT_MEDIAN        = 1992
-LST                   = 3457
-SOIL_MOISTURE         = 7382
-SOYBEANS              =  270
-COFFEE                =  225
-COCOA                 =  211
-COTTON_LINT           =  152
-SUGARCANE             =  538
-CASSAVA               =  237
-BRAZ_ARABICA          = 5157
-POTENT_ETA            = 5072
-RAINFED_CROPLAND      = 7815
-MOISTURE              = 7382
-AVOCADOS              =  127
+# ITEM_ID GLOBALS
+NDVI_DIFF_10YR_MEAN = 409
+ETA_PCT_MEDIAN = 1992
+LST = 3457
+SOIL_MOISTURE = 7382
+SOYBEANS = 270
+COFFEE = 225
+COCOA = 211
+COTTON_LINT = 152
+SUGARCANE = 538
+CASSAVA = 237
+BRAZ_ARABICA = 5157
+POTENT_ETA = 5072
+RAINFED_CROPLAND = 7815
+MOISTURE = 7382
+AVOCADOS = 127
 
 
-#REGION_ID GLOBALS
-WORLD                 =      0
-BRAZIL                =   1029
-KENYA_NAIROBI         =  13474
-UNITED_STATES         =   1215
-SAO_PAULO             =  10408
-MATO_GROSSO           =  10417
+# REGION_ID GLOBALS
+WORLD = 0
+BRAZIL = 1029
+KENYA_NAIROBI = 13474
+UNITED_STATES = 1215
+SAO_PAULO = 10408
+MATO_GROSSO = 10417
 
-#SOURCE_ID GLOBALS
-ICCO        = 13
-ICO        =  9
-UNICA       = 47
-CEPEA       = 74
-GIMMS_MODIS =  3
-LST_SRC     = 26
-FEWSNET1D   = 44
-FEWSNET1M   = 18
-GLOBCOVER   = 53
-SMOS        = 43
-NAFIS       = 12
-GROYM       = 32
+# SOURCE_ID GLOBALS
+ICCO = 13
+ICO = 9
+UNICA = 47
+CEPEA = 74
+GIMMS_MODIS = 3
+LST_SRC = 26
+FEWSNET1D = 44
+FEWSNET1M = 18
+GLOBCOVER = 53
+SMOS = 43
+NAFIS = 12
+GROYM = 32
 
-#FREQUENCY_ID GLOBALS
-DAILY                 =  1
-WEEKLY                =  2
-EIGHTDAY              =  3
-SIXTEENDAY            =  5
-MONTHLY               =  6
-QUARTERLY             =  7
-ANNUAL                =  9
-POINT_IN_TIME         = 15
+# FREQUENCY_ID GLOBALS
+DAILY = 1
+WEEKLY = 2
+EIGHTDAY = 3
+SIXTEENDAY = 5
+MONTHLY = 6
+QUARTERLY = 7
+ANNUAL = 9
+POINT_IN_TIME = 15
 
-#Set up gro API
-def groToken( api_host = API_HOST ):
-    return os.environ['GROAPI_TOKEN']
+# Set up gro API
 
-def groClient( api_host = API_HOST, tok = groToken() ):
-    return( api.client.Client(API_HOST, tok ) )
-
-client = groClient()
+client = GroClient(API_HOST, ACCESS_TOKEN)
 
 # BRAZIL COCOA GRINDINGS FROM ICCO (1)
-x01 = get_df( client, metric_id    = GRINDINGS,
-                      item_id      = COCOA,
-                      region_id    = BRAZIL,
-                      frequency_id = ANNUAL,
-                      source_id    = ICCO )
+print('BRAZIL COCOA GRINDINGS FROM ICCO')
+x01 = get_df(client, metric_id=GRINDINGS,
+             item_id=COCOA,
+             region_id=BRAZIL,
+             frequency_id=ANNUAL,
+             source_id=ICCO)
+print(x01.head())
 
 # BRAZIL ARABICA COMPOSITE PRICES (2)
-x02 = get_df( client, metric_id    = COMPOSITE_PRICES,
-                      item_id      = BRAZ_ARABICA,
-                      region_id    = WORLD,
-                      frequency_id = DAILY,
-                      source_id    = ICO )
+print('BRAZIL ARABICA COMPOSITE PRICES')
+x02 = get_df(client, metric_id=COMPOSITE_PRICES,
+             item_id=BRAZ_ARABICA,
+             region_id=WORLD,
+             frequency_id=DAILY,
+             source_id=ICO)
+print(x01.head())
 
 # SAO PAULO SUGAR AREA FROM UNICA (3)
-x03 = get_df( client, metric_id    = HARV_AREA,
-                      item_id      = SUGARCANE,
-                      region_id    = SAO_PAULO,
-                      frequency_id = ANNUAL,
-                      source_id    = UNICA )
+print('SAO PAULO SUGAR AREA FROM UNICA')
+x03 = get_df(client, metric_id=HARV_AREA,
+             item_id=SUGARCANE,
+             region_id=SAO_PAULO,
+             frequency_id=ANNUAL,
+             source_id=UNICA)
+print(x01.head())
 
 # SAO PAULO CASSAVA PRICES FROM CEPEA (4)
-x04 = get_df( client, metric_id    = PRODUCER_PRICE,
-                      item_id      = CASSAVA,
-                      region_id    = SAO_PAULO,
-                      frequency_id = WEEKLY,
-                      source_id    = CEPEA )
+print('SAO PAULO CASSAVA PRICES FROM CEPEA')
+x04 = get_df(client, metric_id=PRODUCER_PRICE,
+             item_id=CASSAVA,
+             region_id=SAO_PAULO,
+             frequency_id=WEEKLY,
+             source_id=CEPEA)
+print(x01.head())
 
 # NDVI ANOMALY SAO_PAULO (5)
-x05 = get_df( client, metric_id    = VEG_ANOMALIES,
-                      item_id      = NDVI_DIFF_10YR_MEAN,
-                      region_id    = SAO_PAULO,
-                      frequency_id = SIXTEENDAY,
-                      source_id    = GIMMS_MODIS )
+print('NDVI ANOMALY SAO_PAULO')
+x05 = get_df(client, metric_id=VEG_ANOMALIES,
+             item_id=NDVI_DIFF_10YR_MEAN,
+             region_id=SAO_PAULO,
+             frequency_id=SIXTEENDAY,
+             source_id=GIMMS_MODIS)
+print(x01.head())
 
 # LAND SURFACE TEMPERATURE SAO PAULO (6)
-x06 = get_df( client, metric_id    = TEMPERATURE,
-                      item_id      = LST,
-                      region_id    = SAO_PAULO,
-                      frequency_id = DAILY,
-                      source_id    = LST_SRC )
+print('LAND SURFACE TEMPERATURE SAO PAULO')
+x06 = get_df(client, metric_id=TEMPERATURE,
+             item_id=LST,
+             region_id=SAO_PAULO,
+             frequency_id=DAILY,
+             source_id=LST_SRC)
+print(x01.head())
 
 # DAILY EVAPOTRANSPIRATION SAO PAULO (7)
-x07 = get_df( client, metric_id    = ET_VALUE,
-                      item_id      = POTENT_ETA,
-                      region_id    = SAO_PAULO,
-                      frequency_id = DAILY,
-                      source_id    = FEWSNET1D )
+print('DAILY EVAPOTRANSPIRATION SAO PAULO')
+x07 = get_df(client, metric_id=ET_VALUE,
+             item_id=POTENT_ETA,
+             region_id=SAO_PAULO,
+             frequency_id=DAILY,
+             source_id=FEWSNET1D)
+print(x01.head())
 
 # MONTHLY EVAPOTRANSPIRATION ANOMALY SAO_PAULO (8)
-x08 = get_df( client, metric_id    = ETA_PCT,
-                      item_id      = ETA_PCT_MEDIAN,
-                      region_id    = SAO_PAULO,
-                      frequency_id = MONTHLY,
-                      source_id    = FEWSNET1M )
+print('MONTHLY EVAPOTRANSPIRATION ANOMALY SAO_PAULO')
+x08 = get_df(client, metric_id=ETA_PCT,
+             item_id=ETA_PCT_MEDIAN,
+             region_id=SAO_PAULO,
+             frequency_id=MONTHLY,
+             source_id=FEWSNET1M)
+print(x01.head())
 
 # GLOBCOVER RAINFED CROPLAND SAO_PAULO (9)
-x09 = get_df( client, metric_id    = LAND_COVER,
-                      item_id      = RAINFED_CROPLAND,
-                      region_id    = SAO_PAULO,
-                      frequency_id = POINT_IN_TIME,
-                      source_id    = GLOBCOVER )
+print('GLOBCOVER RAINFED CROPLAND SAO_PAULO')
+x09 = get_df(client, metric_id=LAND_COVER,
+             item_id=RAINFED_CROPLAND,
+             region_id=SAO_PAULO,
+             frequency_id=POINT_IN_TIME,
+             source_id=GLOBCOVER)
+print(x01.head())
 
 # SOIL MOISTURE SAO_PAULO (10)
-x10 = get_df( client, metric_id    = SOIL_AVAIL,
-                      item_id      = MOISTURE,
-                      region_id    = SAO_PAULO,
-                      frequency_id = DAILY,
-                      source_id    = SMOS )
+print('SOIL MOISTURE SAO_PAULO')
+x10 = get_df(client, metric_id=SOIL_AVAIL,
+             item_id=MOISTURE,
+             region_id=SAO_PAULO,
+             frequency_id=DAILY,
+             source_id=SMOS)
+print(x01.head())
 
 # NAFIS KENYA AVOCADO PRICE (11)
-x11 = get_df( client, metric_id    = WHOLESALE_PX,
-                      item_id      = AVOCADOS,
-                      region_id    = KENYA_NAIROBI,
-                      frequency_id = DAILY,
-                      source_id    = NAFIS )
+print('NAFIS KENYA AVOCADO PRICE')
+x11 = get_df(client, metric_id=WHOLESALE_PX,
+             item_id=AVOCADOS,
+             region_id=KENYA_NAIROBI,
+             frequency_id=DAILY,
+             source_id=NAFIS)
+print(x01.head())
 
 # GRO US SOYBEAN YIELD (12)
-x12 = get_df( client, metric_id    = YIELD,
-                      item_id      = SOYBEANS,
-                      region_id    = UNITED_STATES,
-                      frequency_id = ANNUAL,
-                      source_id    = GROYM )
-
-
-
-
-
-
+print('GRO US SOYBEAN YIELD')
+x12 = get_df(client, metric_id=YIELD,
+             item_id=SOYBEANS,
+             region_id=UNITED_STATES,
+             frequency_id=ANNUAL,
+             source_id=GROYM)
+print(x01.head())
--- api/client/samples/quick_start.py
+++ api/client/samples/quick_start.py
@@ -1,24 +1,26 @@
 import os
 import unicodecsv
-import api.client.lib
+from api.client.gro_client import GroClient
 
 API_HOST = 'api.gro-intelligence.com'
 OUTPUT_FILENAME = 'gro_client_output.csv'
-ACCESS_TOKEN=os.environ['GROAPI_TOKEN']
+ACCESS_TOKEN = os.environ['GROAPI_TOKEN']
+
 
 def main():
-    client = api.client.Client(API_HOST, ACCESS_TOKEN)
+    client = GroClient(API_HOST, ACCESS_TOKEN)
 
-    selected_entities = { u'region_id': 1210, # Ukraine
-                          u'item_id': 95, # Wheat
-                          u'metric_id': 570001 } # Area Harvested (area)
+    selected_entities = {u'region_id': 1210,  # Ukraine
+                         u'item_id': 95,  # Wheat
+                         u'metric_id': 570001}  # Area Harvested (area)
 
     writer = unicodecsv.writer(open(OUTPUT_FILENAME, 'wb'))
 
     # Get what possible series there are for that combination of selections
     for data_series in client.get_data_series(**selected_entities):
-        
-        # Add a time range restriction to your data request (Optional - otherwise get all points)
+
+        # Add a time range restriction to your data request
+        # (Optional - otherwise get all points)
         data_series['start_date'] = '2000-01-01'
         data_series['end_date'] = '2012-12-31'
 
