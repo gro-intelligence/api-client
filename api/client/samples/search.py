d099db20a5fc8be1fe5df9fcf32c4725017a382c
John Ward <john.ward@gro-intelligence.com>
2019-04-26 20:54:00 +0000

    Update search_and_lookup example to search for CASDE and expand comments

--- api/client/samples/search.py
+++ api/client/samples/search.py
@@ -30,14 +30,24 @@ ACCESS_TOKEN = os.environ['GROAPI_TOKEN']
 def main():
     client = GroClient(API_HOST, ACCESS_TOKEN)
 
-    # See what sources match the "Gro" search term
-    for source in client.search_and_lookup('sources', 'Gro'):
+    # We can search for metrics, items, regions, or sources by name. Let's
+    # say we're interested in finding more information about the source
+    # CASDE (China Agricultural Supply and Demand Estimates). We can search
+    # for it by name to find its id, description, historical start date, etc.
+    for source in client.search_and_lookup('sources', 'CASDE'):
         print(source)
 
-    # Take the best-matching source for "Gro Yield Model"
+    # Now, let's say we are interested in seeing what metrics/items/regions
+    # Gro publishes its own yield model values for. "Gro Yield Model" is
+    # treated as its own source just like any external source, and you can find
+    # it in the same manner. This time we are using the `search` function
+    # instead of `search_and_lookup()` since we only need the id and none of
+    # that extra information mentioned above.
+    # Search results are returned in order of relevance, so here we will take
+    # the first one that is returned:
     source = client.search('sources', 'Gro Yield Model')[0]
 
-    # See what data series are available for that source
+    # Now we can see what data series are available for that source:
     for data_series in client.get_data_series(**{'source_id': source['id']}):
         print(data_series)
 
