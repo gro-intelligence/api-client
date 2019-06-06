678a188fabe963aac157e2c684052a35a5dd80ae
John Ward <john.ward@gro-intelligence.com>
2019-06-06 17:17:41 +0000

    Fix similar regions import and add testing dependencies to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 import setuptools
+import sys
+
+needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
+pytest_runner = ['pytest-runner'] if needs_pytest else []
 
 with open("README.md", "r") as readme_file:
     long_description = readme_file.read()
 
 with open("requirements.txt", "r") as requirements_file:
     requirements = requirements_file.read()
+
+with open("test-requirements.txt", "r") as test_requirements_file:
+    test_requirements = test_requirements_file.read()
 
 setuptools.setup(
     name="gro",
@@ -17,6 +24,9 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     python_requires=">=2.7.6",
     install_requires=requirements,
+    setup_requires=pytest_runner,
+    test_suite='pytest',
+    tests_require=test_requirements,
     entry_points={
         'console_scripts': ['gro_client=api.client.gro_client:main']
     }
