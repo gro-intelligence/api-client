75a2798c614174a3f6b2749d4571fad471c7433d
John Ward <john.ward@gro-intelligence.com>
2019-11-18 21:18:56 +0000

    Move rank_series_by_source description up to Client

--- api/client/__init__.py
+++ api/client/__init__.py
@@ -150,12 +150,7 @@ class Client(object):
     def search_and_lookup(self, entity_type, search_terms, num_results=10):
         """Search for the given search terms and look up their details.
 
-        For each result, yield a dict of the entity and it's properties:
-        { 'id': <integer id of entity, unique within this entity type>,
-            'name':    <string canonical name>
-            'contains': <array of ids of entities that are contained in this one>,
-            ....
-            <other properties> }
+        For each result, yield a dict of the entity and it's properties.
 
         Parameters
         ----------
@@ -210,6 +205,21 @@ class Client(object):
                                   entity_type, entity_id)
 
     def rank_series_by_source(self, series_list):
+        """Given a list of series selections, for each unique combination excluding source, expand
+        to all available sources and return them in ranked order. The order corresponds to how well
+        that source covers the selection (metrics, items, regions, and time range and frequency).
+
+        Parameters
+        ----------
+        series_list : list of dicts
+            See the output of get_data_series().
+
+        Yields
+        ------
+        dict
+            The input series_list, expanded out to each possible source, ordered by coverage.
+
+        """
         return lib.rank_series_by_source(self.access_token, self.api_host,
                                          series_list)
 
