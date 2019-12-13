e0c55e5982c8ec05da7d1d6dbcd8b66b1493230c
John Ward <john.ward@gro-intelligence.com>
2019-12-13 19:33:22 +0000

    Remove source_id from test output

--- api/client/lib_test.py
+++ api/client/lib_test.py
@@ -140,7 +140,7 @@ def test_get_data_points(mock_requests_get):
         'region_id': None,
         'partner_region_id': 0,
         'frequency_id': None,
-        'source_id': None,
+        # 'source_id': None, TODO: add source to output
         'belongs_to': {}
     }]
     initialize_requests_mocker_and_get_mock_data(mock_requests_get,
