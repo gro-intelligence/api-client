21e9b5d55f0922112683ebb1ae22281eab5aaf2e
saikat <joysaikat@gmail.com>
2019-12-19 15:48:12 -0500

    Fixed an import statement for remote branch

--- api/client/samples/analogous_years/run_analogous_years.py
+++ api/client/samples/analogous_years/run_analogous_years.py
@@ -5,8 +5,8 @@ import os
 
 from api.client.gro_client import GroClient
 
-# from api.client.samples.analogous_years.lib import final_ranks_computation
-from lib import final_ranks_computation
+from api.client.samples.analogous_years.lib import final_ranks_computation
+# from lib import final_ranks_computation
 
 
 def str2bool(v):
