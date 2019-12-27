df4244fd6e910824b7b432ccd36bae67ed4402b2
saikat <joysaikat@gmail.com>
2019-12-26 21:03:14 -0500

    Corrected a naming convention: Replaced entity with data_series where necessary

--- api/client/samples/analogous_years/lib/final_ranks_computation.py
+++ api/client/samples/analogous_years/lib/final_ranks_computation.py
@@ -20,65 +20,65 @@ from api.client.samples.analogous_years.lib import \
     get_transform_data
 
 
-def common_start_date(client, entities, provided_start_date=None):
-    """Computes the earliest available start date from which all gro-entities have data"""
+def common_start_date(client, data_series, provided_start_date=None):
+    """Computes the earliest available start date from which all gro-data_series have data"""
     logger = client.get_logger()
     start_date_list = []
     if provided_start_date:
         start_date_list.append(provided_start_date)
-    for i in range(len(entities)):
-        dates = client.get_data_points(**entities[i])
+    for i in range(len(data_series)):
+        dates = client.get_data_points(**data_series[i])
         if len(dates) == 0:
-            msg = "No data found for the following gro-entity - {}".format(entities[i])
+            msg = "No data found for the following gro-data_series - {}".format(data_series[i])
             logger.warning(msg)
             raise Exception
         else:
             start_date_list.append(dates[0]['start_date'])
     start_date = max(start_date_list)
-    for i in range(len(entities)):
-        entities[i]['start_date'] = start_date
-    return {'entities': entities, 'start_date': start_date}
+    for i in range(len(data_series)):
+        data_series[i]['start_date'] = start_date
+    return {'data_series': data_series, 'start_date': start_date}
 
 
 def enso_data(start_date):
-    enso_entity = {'metric_id': 15851977,
-                   'item_id': 13495,
-                   'region_id': 0,
-                   'source_id': 124,
-                   'start_date': start_date,
-                   'frequency_id': 6}
-    return enso_entity
+    enso_data_series = {'metric_id': 15851977,
+                        'item_id': 13495,
+                        'region_id': 0,
+                        'source_id': 124,
+                        'start_date': start_date,
+                        'frequency_id': 6}
+    return enso_data_series
 
 
-def get_file_name(client, entity_list, initial_date, final_date):
+def get_file_name(client, data_series_list, initial_date, final_date):
     """Combines region, items, and dates to return a string"""
-    key_words = [client.lookup('regions', entity_list[0]['region_id'])['name']]
-    for i in range(len(entity_list)):
-        key_words.append(client.lookup('items', entity_list[i]['item_id'])['name'])
+    key_words = [client.lookup('regions', data_series_list[0]['region_id'])['name']]
+    for i in range(len(data_series_list)):
+        key_words.append(client.lookup('items', data_series_list[i]['item_id'])['name'])
     key_words.append(initial_date)
     key_words.append(final_date)
     combined_name = '_'.join(key_words)
     return combined_name
 
 
-def time_series(client, entity, initial_date, final_date):
+def time_series(client, data_series, initial_date, final_date):
     """
-    retrieves a sub-time series of a time series associated with a gro entity
+    retrieves a sub-time series of a time series associated with a gro data_series
     :param client: GroClient
-    :param entity: A dictionary of GroEntity
+    :param data_series: A dictionary of Gro data_series
     :param final_date: 'YYYY-MM-DD'
     :param initial_date: 'YYYY-MM-DD'
     :return: A dataframe with data from the relevant dates
     """
     logger = client.get_logger()
-    properties = {'metric_id', 'item_id', 'region_id', 'source_id', 'frequency_id', 'start_date'}
+    entities = {'metric_id', 'item_id', 'region_id', 'source_id', 'frequency_id', 'start_date'}
     discard = []
-    for prop in entity:
-        if prop not in properties:
-            discard.append(prop)
-    for prop in discard:
-        entity.pop(prop)
-    data = get_transform_data.get_data(client, **entity)
+    for entity_type in data_series:
+        if entity_type not in entities:
+            discard.append(entity_type)
+    for entity_type in discard:
+        data_series.pop(entity_type)
+    data = get_transform_data.get_data(client, **data_series)
     consolidated_data = get_transform_data.combine_subregions(data)
     try:
         ts = get_transform_data.extract_time_periods_by_dates(consolidated_data,
@@ -87,7 +87,7 @@ def time_series(client, entity, initial_date, final_date):
         return ts
     except Exception as e:
         message = ('Please check availability of data for {}'.format(client.lookup(
-            'items', entity['item_id'])['name']))
+            'items', data_series['item_id'])['name']))
         logger.warning(message)
         raise e
 
@@ -113,19 +113,19 @@ def ay_dtw(timeseries):
     return distance_matrix.dtw_dist_matrix(pivot_time_series)
 
 
-def ranked_df_dictionary(client, entity, initial_date, final_date, item, methods_list):
+def ranked_df_dictionary(client, data_series, initial_date, final_date, item, methods_list):
     """
     Returns a dictionary whose keys are given by a 'method_item' and values are the dataframes
     of distances computed for that item using that method. Example: key: cumulative_Rainfall
     :param client: GroClient
-    :param entity: A dictionary of Gro entity
+    :param data_series: A dictionary of Gro data_series
     :param initial_date: 'YYYY-MM-DD'
     :param final_date: 'YYYY-MM-DD'
     :param item: Gro Item
     :param methods_list: a sublist of ['cumulative', 'euclidean', 'ts-features', 'dtw']
     :return: Dictionary of dataframes
     """
-    ts = time_series(client, entity, initial_date, final_date)
+    ts = time_series(client, data_series, initial_date, final_date)
     dictionary_of_methods = {'cumulative_' + item: ay_cumulative,
                              'euclidean_' + item: ay_euclidean,
                              'dtw_' + item: ay_dtw,
@@ -160,20 +160,20 @@ def combined_methods_distances(dictionary_of_df):
     return rank_df
 
 
-def analogous_years(client, entities, initial_date, final_date,
+def analogous_years(client, data_series_list, initial_date, final_date,
                     methods_list=['euclidean', 'cumulative', 'ts-features'],
                     all_ranks=None, weights=None, enso=None, enso_weight=None,
                     provided_start_date=None):
     """
-    Use L^2 distance function to combine weighted distances from multiple gro-entities
+    Use L^2 distance function to combine weighted distances from multiple gro-data_series
     and return the rank
     :param client: Gro_client
-    :param entities: list of dictionaries containing gro entities
+    :param data_series_list: list of dictionaries containing gro data series
     :param initial_date: A date in YYYY-MM-DD format
     :param final_date: A date in YYYY-MM-DD format
     :param methods_list: a sublist of ['cumulative', 'euclidean', 'ts-features', 'dtw']
     :param all_ranks: Boolean to determine if all ranks will be displayed or a composite rank
-    :param weights: Float determining the weight given to each entity
+    :param weights: Float determining the weight given to each data_series
     :param enso: Boolean to include ENSO
     :param enso_weight: Float
     :param provided_start_date: A string in YYYY-MM-DD format
@@ -183,22 +183,22 @@ def analogous_years(client, entities, initial_date, final_date,
     """
     logger = client.get_logger()
     combined_items_distances = None
-    entities = common_start_date(client, entities, provided_start_date)['entities']
-    start_date = common_start_date(client, entities, provided_start_date)['start_date']
+    data_series_list = common_start_date(client, data_series_list, provided_start_date)['data_series']
+    start_date = common_start_date(client, data_series_list, provided_start_date)['start_date']
     if not weights:
-        weights = [1] * len(entities)
+        weights = [1] * len(data_series_list)
     if enso:
-        entities.append(enso_data(start_date))
+        data_series_list.append(enso_data(start_date))
         if enso_weight:
             weights.append(enso_weight)
         else:
             weights.append(1)
-    file_name = get_file_name(client, entities, initial_date, final_date)
-    for i in range(len(entities)):
-        gro_item = client.lookup('items', entities[i]['item_id'])['name']
+    file_name = get_file_name(client, data_series_list, initial_date, final_date)
+    for i in range(len(data_series_list)):
+        gro_item = client.lookup('items', data_series_list[i]['item_id'])['name']
         combined_methods_distances_df = combined_methods_distances(
             ranked_df_dictionary(
-                client, entities[i], initial_date,
+                client, data_series_list[i], initial_date,
                 final_date, gro_item, methods_list))
         numpy_combined_methods_distances = combined_methods_distances_df.values
         if combined_items_distances is None:
