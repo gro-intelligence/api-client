21fae1f719822976f7c973648d05fb709a09efae
Nemo Semret <nemo@gro-intelligence.com>
2020-01-10 15:06:47 +0300

    updated outputs

--- api/client/cfg.py
+++ api/client/cfg.py
@@ -3,7 +3,7 @@ import logging
 
 ASYNC=False
 MAX_RETRIES = 4
-DEFAULT_LOG_LEVEL = logging.INFO  # change to DEBUG for more detail
+DEFAULT_LOG_LEVEL = logging.WARNING  # change to DEBUG for more detail
 MAX_QUERIES_PER_SECOND = 10
 CROP_CALENDAR_METRIC_ID = 2260063
 TIMEOUT=6000
