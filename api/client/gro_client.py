b4fa63afeabf5fe72e2a81bfc7160818746767f1
Nemo Semret <nemo@gro-intelligence.com>
2020-03-16 18:19:15 -0400

    limit to consume only the first N data_series fro get_data_series
     get_data_series() should be a generator (i.e. yields rather than return) so it works as a stream of results for the caller to consume as long as they need, so there would be no need for a fixed limit to response size (obviously the server side will have limits). however recent changes made it return a list intead of a generator... so this prevents find_data_series from being taking too long. we want this number to be large though since not finding the correct result = infinite time ;)

--- api/client/gro_client.py
+++ api/client/gro_client.py
@@ -384,7 +384,7 @@ class GroClient(Client):
         ranking_groups = set()
         count = 0
         for comb in itertools.product(*results):
-            for data_series in self.get_data_series(**dict(comb)):
+            for data_series in self.get_data_series(**dict(comb))[:cfg.MAX_SERIES_PER_COMB]:
                 count += 1
                 self._logger.debug("Data series: {}".format(data_series))
                 # remove time and frequency to rank them
