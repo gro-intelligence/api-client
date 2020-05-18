8e206a874cac1b2008096ab039dab03da7bd5fb3
John Ward <john.ward@gro-intelligence.com>
2020-05-18 20:50:02 +0000

    test: add unit test for Batch error handling

--- api/client/batch_client_test.py
+++ api/client/batch_client_test.py
@@ -10,10 +10,10 @@ from unittest import TestCase
 import json
 from datetime import date
 
-from tornado.httpclient import HTTPResponse
+from tornado.httpclient import HTTPResponse, HTTPError
 from tornado.concurrent import Future
 
-from api.client.batch_client import BatchClient
+from api.client.batch_client import BatchClient, BatchError
 
 MOCK_HOST = 'pytest.groclient.url'
 MOCK_TOKEN = 'pytest.groclient.token'
@@ -38,32 +38,49 @@ mock_list_of_series_points = [
     }
 ]
 
+mock_data_series = [
+    {
+        'metric_id': 860032,  # TODO: add names
+        'item_id': 274,
+        'region_id': 1215,
+        'partner_region_id': 0,
+        'frequency_id': 9,
+        'source_id': 2
+    }, {
+        'metric_id': 860032,  # TODO: add names
+        'item_id': 274,
+        'region_id': 1216,
+        'partner_region_id': 0,
+        'frequency_id': 9,
+        'source_id': 2
+    }
+]
+
+ERROR_SELECTION = {
+    'metric_id': 1,
+    'item_id': -15,
+    'region_id': 3,
+    'frequency_id': 4,
+    'source_id': 5
+}
 
 def mock_rank_series_by_source(access_token, api_host, selections_list):
-    mock_data_series = [
-        {
-            'metric_id': 860032,  # TODO: add names
-            'item_id': 274,
-            'region_id': 1215,
-            'partner_region_id': 0,
-            'frequency_id': 9,
-            'source_id': 2
-        }, {
-            'metric_id': 860032,  # TODO: add names
-            'item_id': 274,
-            'region_id': 1216,
-            'partner_region_id': 0,
-            'frequency_id': 9,
-            'source_id': 2
-        }
-    ]
     for data_series in mock_data_series:
         yield data_series
 
 
 def mock_tornado_fetch(request):
     future = Future()
-    response = HTTPResponse(request, 200, buffer=StringIO(json.dumps(mock_list_of_series_points)))
+    error_url = ('https://{}/v2/data?metricId={metric_id}'
+                 '&itemId={item_id}'
+                 '&regionId={region_id}'
+                 '&frequencyId={frequency_id}'
+                 '&sourceId={source_id}'
+                 '&responseType=list_of_series'.format(MOCK_HOST, **ERROR_SELECTION))
+    if request.url == error_url:
+        raise HTTPError(400, 'Negative item ids are not allowed', request)
+    else:
+        response = HTTPResponse(request, 200, buffer=StringIO(json.dumps(mock_list_of_series_points)))
     future.set_result(response)
     return future
 
@@ -118,8 +135,9 @@ class GroClientTests(TestCase):
 
         self.assertEquals(summation, 97680)
 
-    def test_batch_async_rank_series_by_source(self):
-        self.assertTrue(True)
+    def test_batch_async_get_data_points_bad_request_error(self):
+        responses = self.client.batch_async_get_data_points([ERROR_SELECTION])
+        self.assertTrue(isinstance(responses[0], BatchError))
 
     def test_get_df(self):
         client = BatchClient(MOCK_HOST, MOCK_TOKEN)
@@ -134,3 +152,17 @@ class GroClientTests(TestCase):
         self.assertEqual(df.iloc[0]['start_date'].date(), date(2017, 1, 1))
         self.assertEqual(df.iloc[0]['end_date'].date(), date(2017, 12, 31))
         self.assertEqual(df.iloc[0]['value'], 40891)
+
+
+    def test_batch_async_rank_series_by_source(self):
+        list_of_ranked_series_lists = self.client.batch_async_rank_series_by_source(
+            [mock_data_series, mock_data_series]
+        )
+        # There were two inputs, so there should be two outputs:
+        self.assertEquals(len(list_of_ranked_series_lists), 2)
+        for series_list in list_of_ranked_series_lists:
+            # Not necessarily true, but true given the mock_rank_series_by_source() function:
+            self.assertEquals(len(series_list), len(mock_data_series))
+            for series in series_list:
+                self.assertTrue('metric_id' in series)
+                self.assertTrue('source_id' in series)
