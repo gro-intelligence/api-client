ccac17fc490325ba377e79f3b481a88f57617210
John Ward <john.ward@gro-intelligence.com>
2020-05-18 19:33:25 +0000

    refactor: make BatchClient get_df use batch_async_get_data_points

--- api/client/batch_client.py
+++ api/client/batch_client.py
@@ -145,8 +145,9 @@ class BatchClient(GroClient):
         if show_revisions:
             for data_series in self._data_series_queue:
                 data_series['show_revisions'] = True
-        self.batch_async_queue(self.get_data_points, self._data_series_queue, None,
-                               self.add_points_to_df)
+        self.batch_async_get_data_points(self._data_series_queue,
+                                         output_list=self._data_frame,
+                                         map_result=self.add_points_to_df)
         return self._data_frame
 
     # TODO: deprecate  the following  two methods, standardize  on one
