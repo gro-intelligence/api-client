29da5031ad6fb240a0409cf0217b2db960b4b632
Nemo Semret <nemo@gro-intelligence.com>
2020-11-19 17:05:19 -0500

    update warning text to refer to correct module name

--- api/client/batch_client/__init__.py
+++ api/client/batch_client/__init__.py
@@ -9,7 +9,7 @@ logger.warning('''
 
     You are importing modules from deprecated `api` module to access Gro
     Intelligence's API.  Please update your code to import from the `groclient`
-    module instead.  The `api.client.crop_model` module will be removed by 2021-03-31.
+    module instead.  The `api.client.batch_client` module will be removed by 2021-03-31.
 
     Replace: api.client.batch_client.BatchClient
        with: groclient.client.GroClient
