868665f55eca93c664200c59946050c0cffe9a42
john li <john.li@gro-intelligence.com>
2020-12-11 13:31:14 -0500

    feat: add __version__ to module

--- groclient/__init__.py
+++ groclient/__init__.py
@@ -1,2 +1,5 @@
 from groclient.client import GroClient
 from groclient.crop_model import CropModel
+
+# Do a runtime lookup to get the groclient package version info.
+__version__ = lib.get_version_info().get('api-client-version', 'unknown')
